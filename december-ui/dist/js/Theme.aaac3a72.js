d374599d8cabedb68e12de32de77ca4610431e61
frostace <frostace0723@gmail.com>
2020-06-07 00:11:20 -0400

    production ready for Layout page

--- december-ui/dist/js/Theme.aaac3a72.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Theme"],{de93:function(e,n,t){"use strict";t.r(n);var o=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is a Theme page")]),t("DemoBlock")],1)},s=[],a=t("89f2"),c={components:{DemoBlock:a["a"]}},l=c,i=t("2877"),u=Object(i["a"])(l,o,s,!1,null,null,null);n["default"]=u.exports}}]);
-//# sourceMappingURL=Theme.aaac3a72.js.map
