8f96c3f8832310797e4f4269f932b09d18d3e331
frostace <frostace0723@gmail.com>
2020-06-20 01:29:53 -0400

    added french and espanol with google translate api

--- december-ui/common/plugins/vue-i18n.js
+++ december-ui/common/plugins/vue-i18n.js
@@ -3,10 +3,12 @@ import VueI18n from "vue-i18n";
 import store from "../../src/store";
 import { locale as en } from "../config/i18n/en";
 import { locale as ch } from "../config/i18n/ch";
+import { locale as es } from "../config/i18n/es";
+import { locale as fr } from "../config/i18n/fr";
 Vue.use(VueI18n);
 
 let messages = {};
-messages = { ...messages, en, ch };
+messages = { ...messages, en, ch, es, fr };
 // store.getters.getCurrLanguage
 const lang = store.getters.getCurrLanguage || "en";
 const i18n = new VueI18n({
